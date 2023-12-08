d995f9d05d5437cb0281caa4e53ee692eac42d3a
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-08 16:36:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function fizzBuzz() {
+	for(let num=1;num<=20;num++){
+	if(num%3===0){
+		console.log("Fizz");
+	}else if(num%5===0){
+		console.log("Buzz");
+	}else{
+		console.log(num);
+	}
+}
+	
+}
+alert(fizzBuzz());
+
